852af406f324fadc323410758bb6b2a5e700c677
iht <IhateTrains@users.noreply.github.com>
2024-10-05 11:01:19 +0200

    Use Microsoft.CodeAnalysis.CSharp

--- PGCG.commonItems.SourceGenerators/GeneratorExecutionContextExtensions.cs
+++ PGCG.commonItems.SourceGenerators/GeneratorExecutionContextExtensions.cs
@@ -3,21 +3,22 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 #nullable enable
 namespace commonItems.SourceGenerators {
-	public static class GeneratorExecutionContextExtensions {
-		private static readonly DiagnosticDescriptor MissingPartialModifier = new DiagnosticDescriptor(
-			id: "PGCG001",
-			title: "Missing partial modifier",
-			messageFormat: "A partial modifier is required, Serialize method implementation will not be generated",
-			category: "commonItems.SourceGenerators",
-			DiagnosticSeverity.Warning,
-			isEnabledByDefault: true);
+    public static class GeneratorExecutionContextExtensions {
+        private static readonly DiagnosticDescriptor MissingPartialModifier = new DiagnosticDescriptor(
+            id: "PGCG001",
+            title: "Missing partial modifier",
+            messageFormat: "A partial modifier is required, Serialize method implementation will not be generated",
+            category: "commonItems.SourceGenerators",
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true);
 
-		public static void ReportMissingPartialModifier(
-			this GeneratorExecutionContext context,
-			ClassDeclarationSyntax classDeclaration)
-			=> context.ReportDiagnostic(
-				Diagnostic.Create(
-					MissingPartialModifier,
-					classDeclaration.GetLocation()));
-	}
+        public static void ReportMissingPartialModifier(
+            this SourceProductionContext context,  // Use SourceProductionContext instead
+            ClassDeclarationSyntax classDeclaration)
+            => context.ReportDiagnostic(
+                Diagnostic.Create(
+                    MissingPartialModifier,
+                    classDeclaration.GetLocation(),
+                    classDeclaration.Identifier.Text));
+    }
 }
